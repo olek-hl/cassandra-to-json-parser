b051bc1323165ffc5195b4135d8f27bc6a2c3126
Oleksandr Hlushenok <ohlushenok@verasoftlabs.eu>
2023-03-08 15:08:26 +0100

    fix parser

--- src/db/cassandra/queries.js
+++ src/db/cassandra/queries.js
@@ -4,7 +4,7 @@ const { keyspace } = credentials;
 
 const getAllTables = `SELECT table_name FROM system_schema.tables WHERE keyspace_name = '${keyspace}'`;
 const getTableSchema = (tableName) =>
-  `SELECT * FROM system_schema.columns WHERE keyspace_name = '${keyspace}' AND table_name = '${tableName}'`;
+  `SELECT column_name, type, kind FROM system_schema.columns WHERE keyspace_name = '${keyspace}' AND table_name = '${tableName}'`;
 const getTableFirstRow = (tableName) =>
   `SELECT * FROM ${keyspace}.${tableName} LIMIT 1`;
 
